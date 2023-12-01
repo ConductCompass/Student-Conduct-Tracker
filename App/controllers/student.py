a9aeb69e55272e070bd86b03fe56ba6b2f455661
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2023-12-01 00:08:00 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -17,3 +17,10 @@ def search_student(studentID):
 
 def get_all_students():
     return Student.query.all()
+	
+def get_all_students_json():
+    students = Student.query.all()
+    if not students:
+        return []
+    students = [student.get_json() for student in students]
+    return students
